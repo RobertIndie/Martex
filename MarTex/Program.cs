7ff6f8ced79d6cbb145f7b05f41d31583b736735
AaronRobert <Robert.Aaron2017@outlook.com>
2019-01-01 22:16:48 +0800

    init project

---
+++ MarTex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarTex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
